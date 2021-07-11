c6126d441a319533387cc647330679bd979ac48d
Sebastián Sáez <geniointernet@gmail.com>
2021-07-11 01:56:40 -0400

    first commit
    hope last

---
+++ main.js
@@ -0,0 +1,542 @@
+function dolorCambio()
+{
+    if($('#cbdolor').is(":checked"))   
+        $("#cdolor").show();
+    else
+        $("#cdolor").hide();
+}
+
+function ortopneaCambio()
+{
+    if($('#ortopnea').is(":checked"))   
+        $("#cortopnea").show();
+    else
+        $("#cortopnea").hide();
+}
+
+function nicturiaCambio()
+{
+    if($('#nicturia').is(":checked"))   
+        $("#cnicturia").show();
+    else
+        $("#cnicturia").hide();
+}
+
+function oliguriaCambio() {
+    if($('#oliguria').is(":checked"))   
+        $("#coliguria").show();
+    else
+        $("#coliguria").hide();
+}
+
+function palpitacionesCambio() {
+    if($('#palpitaciones').is(":checked"))
+        $("#cpalpitaciones").show();
+    else
+        $("#cpalpitaciones").hide();
+}
+
+function diaforesisCambio() {
+    if($('#diaforesis').is(":checked"))
+        $("#cdiaforesis").show();
+    else
+        $("#cdiaforesis").hide();
+}
+
+function edemaCambio() {
+    if($('#edema').is(":checked"))
+        $("#cedema").show();
+    else
+        $("#cedema").hide();
+}
+
+function sincopeCambio() {
+    if($('#sincope').is(":checked"))
+        $("#csincope").show();
+    else
+        $("#csincope").hide();
+}
+
+function lipotimiaCambio() {
+    if($('#lipotimia').is(":checked"))
+        $("#clipotimia").show();
+    else
+        $("#clipotimia").hide();
+}
+
+function hipotensionposturalCambio() {
+    if($('#hipotensionpostural').is(":checked"))
+        $("#chipotensionpostural").show();
+    else
+        $("#chipotensionpostural").hide();
+}
+
+function hipoCambio() {
+    if($('#hipo').is(":checked"))
+        $("#chipo").show();
+    else
+        $("#chipo").hide();
+}
+
+function sialorreaCambio() {
+    if($('#sialorrea').is(":checked"))
+        $("#csialorrea").show();
+    else
+        $("#csialorrea").hide();
+}
+
+function halitosisCambio() {
+    if($('#halitosis').is(":checked"))
+        $("#chalitosis").show();
+    else
+        $("#chalitosis").hide();
+}
+
+function flatulenciasCambio() {
+    if($('#flatulencias').is(":checked"))
+        $("#cflatulencias").show();
+    else
+        $("#cflatulencias").hide();
+}
+
+function disfagiaCambio() {
+    if($('#disfagia').is(":checked"))
+        $("#cdisfagia").show();
+    else
+        $("#cdisfagia").hide();
+}
+
+function odinofagiaCambio() {
+    if($('#odinofagia').is(":checked"))
+        $("#codinofagia").show();
+    else
+        $("#codinofagia").hide();
+}
+
+function regurgitacionCambio() {
+    if($('#regurgitacion').is(":checked"))
+        $("#cregurgitacion").show();
+    else
+        $("#cregurgitacion").hide();
+}
+
+function pirosisCambio() {
+    if($('#pirosis').is(":checked"))
+        $("#cpirosis").show();
+    else
+        $("#cpirosis").hide();
+}
+
+function saciedadprecozCambio() {
+    if($('#saciedad-precoz').is(":checked"))
+        $("#csaciedad-precoz").show();
+    else
+        $("#csaciedad-precoz").hide();
+}
+
+function nauseasCambio() {
+    if($('#nauseas').is(":checked"))
+        $("#cnauseas").show();
+    else
+        $("#cnauseas").hide();
+}
+
+function vomitoCambio() {
+    if($('#vomito').is(":checked"))
+        $("#cvomito").show();
+    else
+        $("#cvomito").hide();
+}
+
+function constipacionCambio() {
+    if($('#constipacion').is(":checked"))
+        $("#cconstipacion").show();
+    else
+        $("#cconstipacion").hide();
+}
+
+function cambioDeposicionesCambio() {
+    if($('#cambio-deposiciones').is(":checked"))
+        $("#ccambio-deposiciones").show();
+    else
+        $("#ccambio-deposiciones").hide();
+}
+
+function estetorreaCambio() {
+    if($('#estetorrea').is(":checked"))
+        $("#cestetorrea").show();
+    else
+        $("#cestetorrea").hide();
+}
+
+function acoliaCambio() {
+    if($('#acolia').is(":checked"))
+        $("#cacolia").show();
+    else
+        $("#cacolia").hide();
+}
+
+function melenaCambio() {
+    if($('#melena').is(":checked"))
+        $("#cmelena").show();
+    else
+        $("#cmelena").hide();
+}
+
+function hematequeziaCambio() {
+    if($('#hematequezia').is(":checked"))
+        $("#chematequezia").show();
+    else
+        $("#chematequezia").hide();
+}
+
+function rectorragiaCambio() {
+    if($('#rectorragia').is(":checked"))
+        $("#crectorragia").show();
+    else
+        $("#crectorragia").hide();
+}
+
+function pujoTenesmoCambio() {
+    if($('#pujo-tenesmo').is(":checked"))
+        $("#cpujo-tenesmo").show();
+    else
+        $("#cpujo-tenesmo").hide();
+}
+
+function meteorismoCambio() {
+    if($('#meteorismo').is(":checked"))
+        $("#cmeteorismo").show();
+    else
+        $("#cmeteorismo").hide();
+}
+
+function hinchazonAbdominalCambio() {
+    if($('#hinchazon-abdominal').is(":checked"))
+        $("#chinchazon-abdominal").show();
+    else
+        $("#chinchazon-abdominal").hide();
+}
+
+function ictericiaCambio() {
+    if($('#ictericia').is(":checked"))
+        $("#cictericia").show();
+    else
+        $("#cictericia").hide();
+}
+
+function pruritoPalmarCambio() {
+    if($('#prurito-palmar').is(":checked"))
+        $("#cprurito-palmar").show();
+    else
+        $("#cprurito-palmar").hide();
+}
+
+function asterixisCambio() {
+    if($('#asterixis').is(":checked"))
+        $("#casterixis").show();
+    else
+        $("#casterixis").hide();
+}
+
+function telangectasiaPetequiasCambio() {
+    if($('#telangectasia-petequias').is(":checked"))
+        $("#ctelangectasia-petequias").show();
+    else
+        $("#ctelangectasia-petequias").hide();
+}
+
+function topeInspiratorioCambio() {
+    if($('#tope-inspiratorio').is(":checked"))
+        $("#ctope-inspiratorio").show();
+    else
+        $("#ctope-inspiratorio").hide();
+}
+
+function tosCambio() {
+    if($('#tos').is(":checked"))
+        $("#ctos").show();
+    else
+        $("#ctos").hide();
+}
+
+function expectoracionesCambio() {
+    if($('#expectoraciones').is(":checked"))
+        $("#cexpectoraciones").show();
+    else
+        $("#cexpectoraciones").hide();
+}
+
+function hemoptisisCambio() {
+    if($('#hemoptisis').is(":checked"))
+        $("#chemoptisis").show();
+    else
+        $("#chemoptisis").hide();
+}
+
+function disneaCambio() {
+    if($('#disnea').is(":checked"))
+        $("#cdisnea").show();
+    else
+        $("#cdisnea").hide();
+}
+
+function disneaParoxisticaCambio() {
+    if($('#disnea-paroxistica').is(":checked"))
+        $("#cdisnea-paroxistica").show();
+    else
+        $("#cdisnea-paroxistica").hide();
+}
+
+function respiracionRuidosaCambio() {
+    if($('#respiracion-ruidosa').is(":checked"))
+        $("#crespiracion-ruidosa").show();
+    else
+        $("#crespiracion-ruidosa").hide();
+}
+
+function cianosisCambio() {
+    if($('#cianosis').is(":checked"))
+        $("#ccianosis").show();
+    else
+        $("#ccianosis").hide();
+}
+
+function corizaCambio() {
+    if($('#coriza').is(":checked"))
+        $("#ccoriza").show();
+    else
+        $("#ccoriza").hide();
+}
+
+function alcoholCambio() {
+    if($('#alcohol').is(":checked"))
+        $("#calcohol").show();
+    else
+        $("#calcohol").hide();
+}
+
+function tabacoCambio() {
+    if($('#tabaco').is(":checked"))
+        $("#ctabaco").show();
+    else
+        $("#ctabaco").hide();
+}
+
+function drogasCambio() {
+    if($('#drogas').is(":checked"))
+        $("#cdrogas").show();
+    else
+        $("#cdrogas").hide();
+}
+
+function agoCambio() {
+    if($('#AGO').is(":checked"))
+        $("#cAGO").show();
+    else
+        $("#cAGO").hide();
+}
+
+function fiebreCambio() {
+    if($('#fiebre').is(":checked"))
+        $("#cfiebre").show();
+    else
+        $("#cfiebre").hide();
+}
+
+function sudoracionCambio() {
+    if($('#sudoracion').is(":checked"))
+        $("#csudoracion").show();
+    else
+        $("#csudoracion").hide();
+}
+
+function escalofriosCambio() {
+    if($('#escalofrios').is(":checked"))
+        $("#cescalofrios").show();
+    else
+        $("#cescalofrios").hide();
+}
+
+function asteniaCambio() {
+    if($('#astenia').is(":checked"))
+        $("#castenia").show();
+    else
+        $("#castenia").hide();
+}
+
+function adinamiaCambio() {
+    if($('#adinamia').is(":checked"))
+        $("#cadinamia").show();
+    else
+        $("#cadinamia").hide();
+}
+
+function anorexiaCambio() {
+    if($('#anorexia').is(":checked"))
+        $("#canorexia").show();
+    else
+        $("#canorexia").hide();
+}
+
+function bajadepesoCambio() {
+    if($('#baja-de-peso').is(":checked"))
+        $("#cbaja-de-peso").show();
+    else
+        $("#cbaja-de-peso").hide();
+}
+
+function nicturiaCambio() {
+    if($('#nicturia').is(":checked"))
+        $("#cnicturia").show();
+    else
+        $("#cnicturia").hide();
+}
+
+function coluriaCambio() {
+    if($('#coluria').is(":checked"))
+        $("#ccoluria").show();
+    else
+        $("#ccoluria").hide();
+}
+
+function hematuriaCambio() {
+    if($('#hematuria').is(":checked"))
+        $("#chematuria").show();
+    else
+        $("#chematuria").hide();
+}
+
+function orinaTurbiaCambio() {
+    if($('#orina-turbia').is(":checked"))
+        $("#corina-turbia").show();
+    else
+        $("#corina-turbia").hide();
+}
+
+function orinaEspumosaCambio() {
+    if($('#orina-espumosa').is(":checked"))
+        $("#corina-espumosa").show();
+    else
+        $("#corina-espumosa").hide();
+}
+
+function poliaquiuriaCambio() {
+    if($('#poliaquiuria').is(":checked"))
+        $("#cpoliaquiuria").show();
+    else
+        $("#cpoliaquiuria").hide();
+}
+
+function anuriaCambio() {
+    if($('#anuria').is(":checked"))
+        $("#canuria").show();
+    else
+        $("#canuria").hide();
+}
+
+function disuriaCambio() {
+    if($('#disuria').is(":checked"))
+        $("#cdisuria").show();
+    else
+        $("#cdisuria").hide();
+}
+
+function tenesmoPujoVerticalCambio() {
+    if($('#tenesmo-pujo-vertical').is(":checked"))
+        $("#ctenesmo-pujo-vertical").show();
+    else
+        $("#ctenesmo-pujo-vertical").hide();
+}
+
+function anasarcaHidropesiaCambio() {
+    if($('#anasarca-hidropesia').is(":checked"))
+        $("#canasarca-hidropesia").show();
+    else
+        $("#canasarca-hidropesia").hide();
+}
+
+function simetriaCambio() {
+    if($('#simetria').is(":checked"))
+        $("#csimetria").show();
+    else
+        $("#csimetria").hide();
+}
+
+function inflamacionCambio() {
+    if($('#inflamacion').is(":checked"))
+        $("#cinflamacion").show();
+    else
+        $("#cinflamacion").hide();
+}
+
+function impotenciaFuncionalCambio() {
+    if($('#impotencia-funcional').is(":checked"))
+        $("#cimpotencia-funcional").show();
+    else
+        $("#cimpotencia-funcional").hide();
+}
+
+function rigidezCambio() {
+    if($('#rigidez').is(":checked"))
+        $("#crigidez").show();
+    else
+        $("#crigidez").hide();
+}
+
+function periarticularCambio() {
+    if($('#periarticular').is(":checked"))
+        $("#cperiarticular").show();
+    else
+        $("#cperiarticular").hide();
+}
+
+function raquialgiaCambio() {
+    if($('#raquialgia').is(":checked"))
+        $("#craquialgia").show();
+    else
+        $("#craquialgia").hide();
+}
+
+function radiculopatiaCambio() {
+    if($('#radiculopatia').is(":checked"))
+        $("#cradiculopatia").show();
+    else
+        $("#cradiculopatia").hide();
+}
+
+function debilidadCambio() {
+    if($('#debilidad').is(":checked"))
+        $("#cdebilidad").show();
+    else
+        $("#cdebilidad").hide();
+}
+
+function parestesiaCambio() {
+    if($('#parestesia').is(":checked"))
+        $("#cparestesia").show();
+    else
+        $("#cparestesia").hide();
+}
+
+function paresiaCambio() {
+    if($('#paresia').is(":checked"))
+        $("#cparesia").show();
+    else
+        $("#cparesia").hide();
+}
+
+$('input#name').keyup(function(){
+    $('#paciente-title').text( this.value );
+    $('#descripcion').hide();
+  });
+
+$('input[type="text"]').each(function() {
+    var elementoFinal = '<span id="' + $(this).attr('id') + '" class="printOnly" ></span>';
+    $(this).after(elementoFinal);
+});
+
+$('input[type="text"]').keyup(function(){
+    var loquedice = $(this).val();
+    $('span#' + $(this).attr('id')).text(loquedice);
+});
---
+++ print.js
@@ -0,0 +1,9 @@
+import { jsPDF } from "jspdf";
+
+// Default export is a4 paper, portrait, using millimeters for units
+
+function generarPDF() {
+    const doc = new jsPDF();
+    doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf");
+}
